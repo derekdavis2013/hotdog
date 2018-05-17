5d5ed626d8135d9848dc62919aaaccdbe2406f9a
Derek Davis <ddavis@cbtnuggets.com>
2018-05-17 12:45:37 -0700

    update everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,56 @@
-const { resolve } = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: [
-        "react-hot-loader/patch",
-        "webpack-dev-server/client?http://localhost:8080",
-        "webpack/hot/only-dev-server",
-        "./index.js"
-    ],
-
-    output: {
-        filename: "[name].[hash].js",
-        path: resolve(__dirname, "dist"),
-        publicPath: "/"
-    },
-
-    context: resolve(__dirname, "src"),
-
-    devtool: "inline-source-map",
-
-    devServer: {
-        hot: true,
-        contentBase: resolve(__dirname, "dist"),
-        publicPath: "/",
-        historyApiFallback: true,
-
-        /*added*/
-        disableHostCheck: true
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: ["babel-loader"],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(scss)$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-                use: "url-loader?limit=100000"
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        }),
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-        }),
-        new webpack.IgnorePlugin(/^(net|http|child_process|timers|dns|fs|zlib)$/)
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            use: ["babel-loader"],
+            exclude: /node_modules/
+        },
+        {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader"
+              }
+            ]
+        },
+        {
+            test: /\.(scss)$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        },
+        {
+            test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+            use: "url-loader?limit=100000"
+        },
+        {
+            test: /\.(webm|mov|mp4)$/,
+            use: [
+                'file-loader'
+            ]
+        }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 };
