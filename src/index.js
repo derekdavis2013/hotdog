414fe8940c5f7b2aee3239ce2b27a8aeda4b39ba
Derek Davis <ddavis@cbtnuggets.com>
2018-05-17 12:14:53 -0700

    Remove redux and add button

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import { Provider } from "react-redux";
-import createHistory from "history/createBrowserHistory";
-import { ConnectedRouter, routerMiddleware } from "react-router-redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-import { Route } from "react-router-dom";
-import rootReducer from "./state/reducers"
-import Home from "./Components/Home"
+import Home from "./Components/Home";
 import "./main.scss";
 
-const history = createHistory();
-
-const middleware = composeWithDevTools(
-    applyMiddleware(routerMiddleware(history), thunkMiddleware)
-);
-
-const store = createStore(rootReducer, middleware);
-
-ReactDOM.render(
-<Provider store={store}>
-    <ConnectedRouter history={history}>
-    <div className="flex flex-site flex-col">
-    <Route path="/">
-    <div>
-    <Route exact path="/" component={Home} />
-</div>
-</Route>
-</div>
-</ConnectedRouter>
-</Provider>,
-document.getElementById("root")
-);
+ReactDOM.render(<Home/>, document.getElementById("root"));
